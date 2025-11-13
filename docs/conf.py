377415195ab3b44c2d995a6e9ac6b7826056b022
Alex Raichev <araichev@mrcagney.com>
2025-11-13 13:28:22 +1300

    Tweaked docs

--- docs/conf.py
+++ docs/conf.py
@@ -53,7 +53,7 @@ source_suffix = ".rst"
 master_doc = "index"
 
 # General information about the project.
-project = "GTFS Kit"
+project = "GTFS Kit Polars"
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -186,7 +186,7 @@ html_static_path = ["_static"]
 # html_file_suffix = None
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = "GTFSKitdoc"
+htmlhelp_basename = "GTFSKitPolarsdoc"
 
 
 # -- Options for LaTeX output ---------------------------------------------
@@ -204,7 +204,13 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    ("index", "GTFSKit.tex", "GTFS Kit Documentation", "Alexander Raichev", "manual"),
+    (
+        "index",
+        "GTFSKitPolars.tex",
+        "GTFS Kit Polars Documentation",
+        "Alexander Raichev",
+        "manual",
+    ),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -232,7 +238,9 @@ latex_documents = [
 
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
-man_pages = [("index", "gtfskit", "GTFS Kit Documentation", ["Alexander Raichev"], 1)]
+man_pages = [
+    ("index", "gtfskit", "GTFS Kit Polars Documentation", ["Alexander Raichev"], 1)
+]
 
 # If true, show URL addresses after external links.
 # man_show_urls = False
@@ -246,10 +254,10 @@ man_pages = [("index", "gtfskit", "GTFS Kit Documentation", ["Alexander Raichev"
 texinfo_documents = [
     (
         "index",
-        "GTFSKit",
-        "GTFS Kit Documentation",
+        "GTFSKitPolars",
+        "GTFS Kit Polars Documentation",
         "Alexander Raichev",
-        "GTFSKit",
+        "GTFSKitPolars",
         "One line description of project.",
         "Miscellaneous",
     ),
